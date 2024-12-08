1f3a62a84052a9f44c374182b7f16392b5b82ff2
Aliaksei <khadasevich.aleksey@gmail.com>
2024-12-08 16:58:32 +0100

    update workflow

--- src/test/java/org/cvs/tests/context/UIBaseTest.java
+++ src/test/java/org/cvs/tests/context/UIBaseTest.java
@@ -42,11 +42,10 @@ public class UIBaseTest extends VcsMgmtPreconditions {
     private void setUpBrowser() {
         Configuration.timeout = WebTimeouts.IMPLICIT_TIMEOUT;
         Configuration.pageLoadTimeout = WebTimeouts.PAGE_LOAD_TIMEOUT;
-        Configuration.headless = true;
+        Configuration.headless = false;
         Configuration.baseUrl = WEB_HOST;
         log.info("Browser configured");
     }
-
 
     @BeforeMethod
     public void loginAndGetCookie() {
